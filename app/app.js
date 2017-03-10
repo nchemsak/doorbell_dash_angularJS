70c101b5c572330db759076917f056c6c56c30f6
Nick Chemsak <nchemsak@gmail.com>
2017-03-10 15:08:54 -0600

    basic layout

--- app/app.js
+++ app/app.js
@@ -1,3 +1,23 @@
 "use strict";
 
-var app = angular.module("doorbellDashApp", ["ngRoute"]);
+
+let app = angular.module('doorbellDashApp', ['ngRoute']).constant('apiUrl', "http://localhost:8000");
+
+app.config(($locationProvider, $routeProvider, $httpProvider) => {
+
+  // $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+  // $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+  // $httpProvider.defaults.withCredentials = true;
+
+  $routeProvider
+    .when('/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'loginCtrl'
+    })
+    // .when('/login', {
+    //   templateUrl: 'partials/login.html',
+    //   controller: 'loginCtrl'
+    // })
+    .otherwise('/');
+  $locationProvider.hashPrefix('');
+});
